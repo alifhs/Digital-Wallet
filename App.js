1d27115a07eedacb47b26f6c514128cdd2086d14
alifhs <alif.hs@outlook.com>
2022-09-16 00:24:33 +0600

    add sign up page

---
+++ App.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SignUp from './screens/SignUp';
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{title: 'Welcome', headerShown: false}}
+        />
+        {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ constants/test.cpp
@@ -0,0 +1,83 @@
+struct sinfo {
+	char fname[50];
+	char lname[50];
+	char dname[50];
+	int id;
+} st[55];
+
+int               a 
+
+data type         variable
+
+
+sinfo var[20];
+
+var[0].fname
+var[0].lname
+var[0].dname
+var[0].id
+
+var[1].fname
+var[1].dname
+var[1].lname
+var[1].id
+
+
+
+i = 25
+
+void find_rl()
+{
+	int x;
+	printf("Enter the ID Number"
+		" of the student\n");
+	scanf("%d", &x);  4
+    [12].id = 4
+	for (int j = 1; j <= i; j++) {
+		if (x == st[i].id) {
+			printf(
+				"The Students Details are\n");
+			printf(
+				"Name : %s",
+				st[i].fname);
+			printf(
+				" %s\n",
+				st[i].lname);
+            printf(
+				"Student of %s Department\n",
+				st[i].dname);
+		}
+		break;
+	}
+}
+
+
+
+
+
+
+int choice, count;
+    i = 1;
+	while (i) {
+        printf("\n\n");
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
